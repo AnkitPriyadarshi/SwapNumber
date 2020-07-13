8c6fe7ebe02e84ecb5648aa33217239d92b08700
AnkitPriyadarshi <ankitpriyadarshi115@gmail.com>
2020-07-13 17:57:09 +0530

    inital commit

---
+++ SwapNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            Console.WriteLine("enter a=");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter b=");
+            b = Convert.ToInt32(Console.ReadLine());
+            a = a * b;    
+            b = a / b;      
+            a = a / b;    
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
